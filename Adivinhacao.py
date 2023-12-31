0382aa5b324fbbe086c63e9a3781f704b4e5a206
Thiago Batista Barbosa Ribeiro <40309873+JamesRibeiro01@users.noreply.github.com>
2023-12-31 14:54:12 -0300

    Primeiro COMMIT

---
+++ Adivinhacao.py
@@ -0,0 +1,23 @@
+print("*************************************")
+print("Bem vindo ao jogo de Advinhação")
+print("*************************************")
+
+#Definindo a variavel de numero secreto
+
+numero_secreto = 42 
+
+#comando usado para interação com o usuario no console.
+#O valor digitado pelo usuario deve ser capturado por uma variavel, entao definimo o input como valor para a variavel chute
+#todo valor digitado no console é considerado uma string, caso seja passado um "numero" deve ser feito a conversão.
+#Usando a função int(), para realizar a "Conversão"
+chute = int(input("Digite o seu número!\n"))
+
+print("Você digitou", chute)
+
+
+#condiçao IF usado para comparação de dois ou mais valores, condicional.
+#Para comparar dois valores no IF vc deve usar o == para comparação, somente "=" é usado para atribuição
+if (numero_secreto == chute):
+    print("Você acertou!")
+else:
+    print("Você errou!")
