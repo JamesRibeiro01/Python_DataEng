344cb77aad94a45197e9b8e580db3fcded9b7642
Thiago Batista Barbosa Ribeiro <40309873+JamesRibeiro01@users.noreply.github.com>
2023-12-31 15:02:21 -0300

    add linha de comentario

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -17,6 +17,7 @@ print("Você digitou", chute)
 
 #condiçao IF usado para comparação de dois ou mais valores, condicional.
 #Para comparar dois valores no IF vc deve usar o == para comparação, somente "=" é usado para atribuição
+# O codigo IF no final do bloco da condição deve ser iniciado com :
 if (numero_secreto == chute):
     print("Você acertou!")
 else:
